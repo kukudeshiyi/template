c74f4db78b3f1ad3915eee08624d91e3651e080f
liqi.shi <liqi.shi@shopee.com>
2021-03-30 22:08:49 +0800

    feat: 🎸 update project config
    projectRoot/configFile

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ export default class App extends React.Component<PropsType, StateType> {
     return (
       <ErrorBoundary>
         <FormTest />
+        <div className="">123</div>
       </ErrorBoundary>
     );
   }
